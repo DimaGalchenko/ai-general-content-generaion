90bd1db6596115224c00c3808f1caef683f3a9ed
DimaGalchenko <galchenkodima31@gmail.com>
2025-10-21 22:58:37 +0300

    * impl task 1

--- task/t1/image_analysis.py
+++ task/t1/image_analysis.py
@@ -3,10 +3,11 @@ import base64
 from task.client import OpenAIClient
 from task.constants import OPENAI_HOST
 
+
 # https://platform.openai.com/docs/guides/images-vision?api-mode=chat#analyze-images
 # https://platform.openai.com/docs/guides/images-vision?api-mode=chat&format=base64-encoded#analyze-images
 
-#TODO:
+# TODO:
 # You need to analyse these 2 images:
 #   - https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
 #   - in this folder we have 'banner.png', load it as encoded data (see documentation)
@@ -18,4 +19,34 @@ from task.constants import OPENAI_HOST
 # ---
 # In the end load both images (url and base64 encoded 'banner.png'), ask "Generate poem based on images" and se what will happen?
 
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
+
+image_path = "./banner.png"
+
+base64_image = encode_image(image_path)
+
+messages = [
+    {
+        'role': 'user',
+        'content': [
+            {'type': 'text', 'text': 'Generate poem based on images in markdown format'},
+            {'type': 'image_url', "image_url": {
+                "url": "https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg",
+            }},
+            {'type': 'image_url', "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }}
+        ]
+    }
+]
+
+client = OpenAIClient(f"{OPENAI_HOST}/v1/chat/completions")
+
+print(client.call(
+    model='gpt-4.1',
+    print_response=True,
+    messages=messages,
+)['choices'][0]['message']['content'])
---
+++ task/t1/response.md
@@ -0,0 +1,20 @@
+![Elephant before a mountain](https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg)
+*Majestic giant, gentle and grand,  
+Beneath snow-capped peaks, across golden sand.  
+With wisdom and memory old as the hills,  
+Stride quiet and patient, nature’s strength and will.*
+
+---
+
+![Codeus Banner](banner.png)
+*From savannah wild to digital domain,  
+We gather as learners, eager to gain.  
+With Codeus we journey, new skills to excel,  
+A quest for knowledge, together we dwell.*
+
+---
+
+**Nature and knowledge, unwritten and code,  
+Paths intertwining along life’s road.  
+Like elephants mighty, or bright screens aglow,  
+We grow in the sunlight of all we know.**
